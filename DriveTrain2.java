f6bb2bd5538fc040b7c7c05c0f00b89a1b938611
Username123-coder <73372807+Username123-coder@users.noreply.github.com>
2022-02-05 10:41:16 -0800

    Update DriveTrain2.java

--- DriveTrain2.java
+++ DriveTrain2.java
@@ -137,7 +137,7 @@ public class DriveTrain2 extends LinearOpMode {
                 Turn(gamepad1.right_stick_x);
             } else { //driving
                 //Drive(-gamepad1.left_stick_y);
-                mecanum(gamepad1.left_stick_x, -gamepad1.left_stick_y); //to switch mecanum
+                mecanumDrive_Cartesian(gamepad1.left_stick_x, -gamepad1.left_stick_y, Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y)); 
             }
         }
     }
@@ -212,6 +212,44 @@ public class DriveTrain2 extends LinearOpMode {
             1. Pause to prevent current movements to affect any future movements.
             */
        }
+    }
+    
+    public void mecanumDrive_Cartesian(double x, double y, double rotation) {
+        double wheelSpeeds[] = new double[4];
+
+        wheelSpeeds[0] = x + y + rotation;
+        wheelSpeeds[1] = -x + y - rotation;
+        wheelSpeeds[2] = -x + y + rotation;
+        wheelSpeeds[3] = x + y - rotation;
+
+        normalize(wheelSpeeds);
+
+        leftFrontMotor.setPower(wheelSpeeds[0]);
+        rightFrontMotor.setPower(wheelSpeeds[1]);
+        leftRearMotor.setPower(wheelSpeeds[2]);
+        rightRearMotor.setPower(wheelSpeeds[3]);
+    }
+
+    private void normalize(double[] wheelSpeeds) {
+        double maxMagnitude = Math.abs(wheelSpeeds[0]);
+
+        for (int i = 1; i < wheelSpeeds.length; i++)
+        {
+            double magnitude = Math.abs(wheelSpeeds[i]);
+
+            if (magnitude > maxMagnitude)
+            {
+                maxMagnitude = magnitude;
+            }
+        }
+
+        if (maxMagnitude > 1.0)
+        {
+            for (int i = 0; i < wheelSpeeds.length; i++)
+            {
+                wheelSpeeds[i] /= maxMagnitude;
+            }
+        }
     }
 
     void Turn (float speed){
